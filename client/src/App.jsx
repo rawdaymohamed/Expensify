12ef27ad75d4e99e336926038a4d324f15b8767a
Rawda Yasser <rawdaymohamed@gmail.com>
2026-03-24 23:12:55 +0200

    Chore: Add shadcn/ui setup and add basic button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 function App() {
   return (
     <>
       <h1>Welcome to the App!</h1>
+      <Button variant="default" size="lg">
+        Click Me
+      </Button>
     </>
   );
 }
