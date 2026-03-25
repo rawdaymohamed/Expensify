aec607dbff7c0e13cd21d0b017872e3de395cb07
Rawda Yasser <rawdaymohamed@gmail.com>
2026-03-25 23:41:28 +0200

    Feat: Update index.js - add connect to database and add client url to cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
