5ca1166fc5324ce33da9dad5d768f57524e979d9
Rawda Yasser <rawdaymohamed@gmail.com>
2026-04-07 03:40:24 +0200

    Feat: add get transaction api route and controller with pagination

--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createTransaction } from "../controllers/transactionController.js";
+import { createTransaction, getTransactions } from "../controllers/transactionController.js";
 import requireAuth from "../middleware/requireAuth.js";
 import addTransactionSchema from "../validations/transactionValidation.js";
 import { validate } from "../middleware/validate.js";
 
 const router = express.Router();
+
+// Get all transactions for authenticated user (protected)
+router.get("/", requireAuth, getTransactions);
 
 // Create a transaction (protected)
 router.post("/", requireAuth, validate(addTransactionSchema), createTransaction);
